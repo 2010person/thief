b107fad7cac5ca159747ac4a3d7228a2f5e953f0
2010person <116949561+2010person@users.noreply.github.com>
2025-06-19 14:14:37 +0100

    sorted out most of the basic code

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-import flask
-
+import itertools #This is a module in the standard library whicb can iterate to allow for efficiency
+num1 = str(int(input())) #taking inputs from the user
+num2 = str(int(input()))
+num3 = str(int(input()))
+num4 = str(int(input()))
+nums = "1234"
+rearrangement = [" ".join(x) for x in itertools.permutations(nums, 4)] 
+for x in rearrangement:
+    for y in list(x):
+        pass
+ans = []
