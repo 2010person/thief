ecc20b39d26c6eb09fe30940d3a7cb8da181f2d1
2010person <116949561+2010person@users.noreply.github.com>
2025-06-29 12:10:11 +0000

    A tiny change to app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import itertools #This is a module in the standard library whicb can iterate to allow for efficiency
-num1 = str(int(input())) #taking inputs from the user
-num2 = str(int(input()))
-num3 = str(int(input()))
-num4 = str(int(input()))
-nums = "1234"
-rearrangement = [" ".join(x) for x in itertools.permutations(nums, 4)] 
-for y in rearrangement:
-    y 
-    for z in list(y):
-        pass
-ans = []
-print(rearrangement)
+try: 
+    #taking inputs from the user
+    num1 = str(int(input()))
+    num2 = str(int(input()))
+    num3 = str(int(input()))
+    num4 = str(int(input()))
+    nums = "1234"
+    rearrangement = [" ".join(x) for x in itertools.permutations(nums, 4)] 
+    for y in rearrangement:
+        y = 
+        for z in list(y):
+            pass
+    ans = []
+    print(rearrangement)
+except ValueError:
+    print("Ooops, it would seem like you haven't inputted a number, please try again!")
